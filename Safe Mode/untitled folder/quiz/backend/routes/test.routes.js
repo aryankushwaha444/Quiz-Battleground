0305a9784d85ee97f682f77a37d6a1d5fc569235
Aaryan Kushawaha <rupnarayan444@gmail.com>
2026-09-01 11:10:35 +0545

    Delete Safe Mode directory

--- Safe Mode/untitled folder/quiz/backend/routes/test.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/bac', (req, res) => {
-  res.json({ message: '✅ Backend is connected to frontend!' });
-});
-
-export default router;
